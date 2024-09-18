3bc6d76897b229e19f191c843b71097a802fc4b1
Monic Auditya <monicauditya@gmail.com>
2024-09-18 22:22:43 +0530

    h

---
+++ src/consstants.js
@@ -0,0 +1,66 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Monic Auditya';
+
+export const experiences = [
+    {
+        'company': 'Technovation symposium',
+        'role': 'Design Dynamo',
+        'duration': 'March 2024 - April 2024',
+        'logo': 'https://media.licdn.com/dms/image/v2/D4E0BAQGFKZNswtv-Rg/company-logo_200_200/company-logo_200_200/0/1707794033174?e=2147483647&v=beta&t=B6rE8SiHEJIQv90W0q1fglChpudErEFhs3ubw8Wm2_k',
+        'points': [
+            'Involved in dynamic contributions to mobile📱 development project, actively influencing and improving project outcomes.',
+            'Did a great teamwork',
+            'Enhanced product performance through contributions to backend development(responses).',
+                   ],
+        'url': 'https://www.google.com/search?client=ms-android-realme-terr1-rso2&sca_esv=bb3b1024b4da0aca&sxsrf=ADLYWIIk_y9RJjo6tJ5YIs0VUphplsdcVw:1726390464662&q=technovation+rec&udm=2&fbs=AEQNm0DJ7g6q1SUpTw4cD2HgLJVN-DyQln-PEBM_ZXBfF_rmPWjJ_V9ADsx_hReH0g80SYUOf87la3QkT0ivP_gnWJou5OJpPAZVtH4TCyO2dqD4_k2BlgVPEaNHP9kBPfyEyvxzr24UUeNLBvPk17T3Tay0AwlqTVzaWOtLL8MpvXKaL_Cv-XtOQABGBziJ_t-J0-3tWwFAC-buHYsoKRPPdfIv4uirrg&sa=X&ved=2ahUKEwiC847TycSIAxUg1TgGHeTqDFgQtKgLegQIDxAB&biw=360&bih=672&dpr=3#vhid=G9W6siVg6dps3M&vssid=mosaic',
+    },
+    {
+        'company': 'Hackmaggedon',
+        'role': 'Hacker',
+        'duration': 'June 2024 - August 2024',
+        'logo': 'https://hackmageddon.in/static/media/hmglogo.59a751b14efc1643e468.webp',
+        'points': [
+            'Gained experience with project development based on problem statement .',
+            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.',
+            'Gave many creative ideas',
+            'Most valuable experience as a team lead heading to finals'
+        ],
+        'url': 'https://hackmageddon.in/?trk=public_post-text',
+    },
+    {
+        'company': 'Smart India Hackathon',
+        'role': 'Hacker',
+        'duration': 'August 2024 - present',
+        'logo': 'https://yt3.ggpht.com/a/AGF-l7-lMMFxQrGg7DIcGPmGvrwkZKlnjVpsqCtxgA=s900-c-k-c0xffffffff-no-rj-mo',
+        'points': [
+            'Gained experience with project development based on problem statement .',
+            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.',
+            'Gave many creative ideas',
+            'Still on developing stage',
+            'gained exposure in Liscensing,terms and policy'
+    
+        ],
+        'url': 'https://www.sih.gov.in/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
